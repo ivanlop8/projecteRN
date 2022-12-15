81c718456ac08c2e66a4a7e778f3c0e842fa9c66
IvanLopez <ivanlopezcam@gmail.com>
2022-12-15 17:34:29 +0100

    Flexbox1

--- App.js
+++ App.js
@@ -8,70 +8,64 @@
 
 import React from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
-  useColorScheme,
   View,
+  Text,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-import { Provider as PaperProvider,TextInput, Text, Button } from 'react-native-paper';
-
-const saluda2 = () => {
-  return (
-  <View>
-    {saluda("Ivan López")}
-    {saluda("Paco")}
-  </View> 
-  ); 
-}
-
-const saluda = (textAMostrar) => {
-  return <Text style={{color:"blue", fontSize: 25 }}>Hola {textAMostrar}</Text>; 
-} 
-
-const App =  () => {   
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  const isMarvel = true; 
-
-  return (
-    <PaperProvider>
-      {isMarvel && saluda("Spider Man") } 
-      {saluda2()}   
-    </PaperProvider> 
-  );
+const App = () => { 
+    return (
+      
+      <View style={styles.contenidor}>
+          <View style={styles.seccio1}>
+              <Text>Secció 1</Text>
+          </View>
+          <View style={styles.seccio2}>
+            <Text>Secció 2</Text>
+          </View>
+          <View style={styles.seccio3}>
+            <Text>Secció 3</Text>
+          </View>
+      </View>
+    );
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  contenidor: {
+    flex: 1,
+    flexDirection: 'column',
+    borderColor:'purple',
+    borderWidth:5,
   },
-  sectionTitle: {
-    fontSize: 24,
+  seccio1: {
+    flex:1,
+    borderColor:'red',
+    borderWidth:3,
+    fontSize: 12,
     fontWeight: '600',
+    margin: 4,
+    padding: 12,
+    textAlign: 'right',
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  seccio2: {
+    flex:1,
+    borderColor:'green',
+    borderWidth:3,
+    fontSize: 12,
+    fontWeight: '600',
+    margin: 4,
+    padding: 12,
+    textAlign: 'center',
+  },  
+  seccio3: {
+    flex:1,
+    borderColor:'blue',
+    borderWidth:3,
+    fontSize: 12,
+    fontWeight: '600',
+    margin: 4,
+    padding: 12,
+    textAlign: 'left',
   },
 });
 
